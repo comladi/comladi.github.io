ad633417f036849a2bcd864aee65231dd9329d93
comladi <133464576+comladi@users.noreply.github.com>
2023-11-07 20:48:11 -0600

    Add files via upload

--- js.js
+++ js.js
@@ -1,2 +1,21 @@
-"use strict"
-document.oncontextmenu = function(){return false}
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll(".slideshow img");
+    let currentSlide = 0;
+
+    function showSlide(slideIndex) {
+        slides.forEach((slide, index) => {
+            slide.classList.remove("active");
+            if (index === slideIndex) {
+                slide.classList.add("active");
+            }
+        });
+    }
+
+    function nextSlide() {
+        showSlide(currentSlide);
+        currentSlide = (currentSlide + 1) % slides.length;
+        setTimeout(nextSlide, 12000); // 12 seconds (7s + 5s)
+    }
+
+    nextSlide();
+});
